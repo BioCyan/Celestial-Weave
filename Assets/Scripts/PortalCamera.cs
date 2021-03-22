24a1b726f9e3b4c8c0ff85b7e5704bd74d42f268
Kevin Jones <bioncl7@gmail.com>
2021-03-22 14:24:39 -0500

    Init

---
+++ Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour {
+	[SerializeField] public GameObject cameraPrefab;
+	[SerializeField] public int portalDepth = 1;
+
+	public GameObject[] leftCameras;
+	public GameObject[] rightCameras;
+
+	private RenderTexture[] leftTextures;
+	private RenderTexture[] rightTextures;
+	private GameObject leftPortal;
+	private GameObject rightPortal;
+
+	void Start() {
+		leftCameras = new GameObject[portalDepth];
+		leftTextures = new RenderTexture[portalDepth];
+		for (int i = 0; i < portalDepth; i++) {
+			leftTextures[i] = new RenderTexture(Screen.width, Screen.height, 24);
+
+			leftCameras[i] = Object.Instantiate(cameraPrefab);
+			leftCameras[i].name = "Left Portal Camera " + (i + 1);
+			leftCameras[i].GetComponent<Camera>().targetTexture = leftTextures[i];
+		}
+
+		rightCameras = new GameObject[portalDepth];
+		rightTextures = new RenderTexture[portalDepth];
+		for (int i = 0; i < portalDepth; i++) {
+			rightTextures[i] = new RenderTexture(Screen.width, Screen.height, 24);
+
+			rightCameras[i] = Object.Instantiate(cameraPrefab);
+			rightCameras[i].name = "Right Portal Camera " + (i + 1);
+			rightCameras[i].GetComponent<Camera>().targetTexture = rightTextures[i];
+		}
+	}
+
+	void Update() {
+		leftPortal = GameObject.FindGameObjectWithTag("Left Portal");
+		rightPortal = GameObject.FindGameObjectWithTag("Right Portal");
+
+		Vector3 pos = this.transform.position;
+		Quaternion rot = this.transform.rotation;
+		Quaternion leftToRight = rightPortal.transform.rotation
+			* Quaternion.Euler(0, 180, 0)
+			* Quaternion.Inverse(leftPortal.transform.rotation);
+		for (int i = 0; i < portalDepth; i++) {
+			pos -= leftPortal.transform.position;
+			pos = leftToRight * pos;
+			pos += rightPortal.transform.position;
+			rot = leftToRight * rot;
+
+			leftCameras[i].transform.position = pos;
+			leftCameras[i].transform.rotation = rot;
+		}
+
+		pos = this.transform.position;
+		rot = this.transform.rotation;
+		Quaternion rightToLeft = leftPortal.transform.rotation
+			* Quaternion.Euler(0, 180, 0)
+			* Quaternion.Inverse(rightPortal.transform.rotation);
+		for (int i = 0; i < portalDepth; i++) {
+			pos -= rightPortal.transform.position;
+			pos = rightToLeft * pos;
+			pos += rightPortal.transform.position;
+			rot = rot * rightToLeft;
+
+			rightCameras[i].transform.position = pos;
+			rightCameras[i].transform.rotation = rot;
+		}
+	}
+
+	void OnPreRender() {
+		Material leftMaterial = leftPortal.GetComponent<Renderer>().material;
+		for (int i = portalDepth - 1; i >= 0; i--) {
+			leftCameras[i].GetComponent<Camera>().Render();
+
+			leftMaterial.mainTexture = leftTextures[i];
+		}
+
+		Material rightMaterial = rightPortal.GetComponent<Renderer>().material;
+		for (int i = portalDepth - 1; i >= 0; i--) {
+			rightCameras[i].GetComponent<Camera>().Render();
+
+			rightMaterial.mainTexture = rightTextures[i];
+		}
+		RenderTexture.active = null;
+	}
+}
---
+++ Assets/Shaders/PortalShader.shader
@@ -0,0 +1,61 @@
+﻿Shader "Unlit/PortalShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                UNITY_FOG_COORDS(1)
+                float4 pos : SV_POSITION;
+                float4 screenPos : TEXCOORD0;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.screenPos = ComputeScreenPos(o.pos);
+                UNITY_TRANSFER_FOG(o,o.pos);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 uv = i.screenPos.xy / i.screenPos.w;
+                uv = TRANSFORM_TEX(uv, _MainTex);
+                
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
