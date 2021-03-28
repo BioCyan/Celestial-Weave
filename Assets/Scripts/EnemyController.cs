2453a4c5a884080430114085bea6bb80735cd290
uvz967 <kingbarrett87@gmail.com>
2021-03-28 12:46:22 -0500

    Edited player tag and Enemy object and movement

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject player;
+    private Rigidbody rigidbody;
+
+    public float speed = 3f;
+    private float distance;
+    private float minDistance = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if(distance <= minDistance)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+    }
+}
