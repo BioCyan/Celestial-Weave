649ba43451b3d8ed9d0b80c165e73ce6c5e2f5cb
johngamal2002 <55814897+johngamal2002@users.noreply.github.com>
2021-03-27 20:33:59 -0500

    added menu and some ui

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+	[SerializeField] bool clicked ;
+
+	// Update is called once per frame
+	void Update()
+    {
+		if(menuButtonController.index == thisIndex)
+		{
+			animator.SetBool ("selected", true);
+			if(Input.GetAxis ("Submit") == 1 || clicked )
+			{
+				animator.SetBool ("pressed", true);
+				if (thisIndex == 0)
+				{
+					menuButtonController.newGame();	
+				}
+				else if (thisIndex == 2) 
+				{
+					menuButtonController.quit();
+					
+				}
+			}
+			else if (animator.GetBool ("pressed"))
+			{
+				animator.SetBool ("pressed", false);
+				animatorFunctions.disableOnce = true;
+			}
+		}
+		else
+		{
+			animator.SetBool ("selected", false);
+		}
+
+	}
+
+	public void mouseOver()
+	{
+		menuButtonController.index = thisIndex;
+	}
+
+	public void mouseClick()
+	{
+		clicked = true;
+	}
+	public void mouseExit()
+	{
+		clicked = false;
+	}
+}
---
+++ Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public int index;
+	[SerializeField] bool keyDown;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
+
+	void Start () 
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetAxis ("Vertical") != 0)
+		{
+			if(!keyDown)
+			{
+				if (Input.GetAxis ("Vertical") < 0) 
+				{
+					if(index < maxIndex)
+					{
+						index++;
+					}
+					else
+					{
+						index = 0;
+					}
+				} 
+				else if(Input.GetAxis ("Vertical") > 0)
+				{
+					if(index > 0)
+					{
+						index --; 
+					}
+					else
+					{
+						index = maxIndex;
+
+					}
+				}
+				keyDown = true;
+			}
+		}
+		else
+		{
+			keyDown = false;
+		}
+	}
+
+	public void PauseGame()
+	{
+		Time.timeScale = 0;
+	}
+
+	public void ResumeGame()
+	{
+		Time.timeScale = 1;
+	}
+	public void quit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+        Application.Quit();
+		#endif
+	}
+
+	public void newGame()
+	{
+		
+		//PlayerPrefs.DeleteKey("PlayerHealth");
+		//PlayerPrefs.DeleteKey("PlayerSpeed");
+		//PlayerPrefs.DeleteKey("Level");
+		SceneManager.LoadScene(1);
+		
+	}
+	private void OnApplicationQuit()
+	{
+		//PlayerPrefs.DeleteKey("PlayerHealth");
+		//PlayerPrefs.DeleteKey("PlayerSpeed");
+		//PlayerPrefs.DeleteKey("Level");
+	}
+
+}
