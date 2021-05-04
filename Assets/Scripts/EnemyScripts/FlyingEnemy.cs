06300ab033f1f76d0c3337bb928e6a52dceb6d21
uvz967 <kingbarrett87@gmail.com>
2021-05-03 20:30:00 -0500

    More work on the enemies

---
+++ Assets/Scripts/EnemyScripts/FlyingEnemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public float health = 50f;
+    public float damage = 15f;
+
+    [SerializeField] private GameObject laserTip;
+    [SerializeField] private LineRenderer laser;
+    private GameObject player;
+
+    private float sightTime = 3f;
+    private float laserRange = 8f;
+
+    void Awake()
+    {
+        if( !laserTip )
+            laserTip = GameObject.Find("LaserTip");
+        if( !laser )
+            laser = GetComponent<LineRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If player comes into range then lock on
+        if( Vector3.Distance(player.transform.position, transform.position) <= laserRange )
+        {
+            LockOnTarget();
+            sightTime -= Time.deltaTime;
+            // If target is within range for more than sightTime(3 sec) then start shooting laser
+            if( sightTime <= 0f )
+                ShootLaser();
+        } else // player is outta range
+        {
+            sightTime = 3f;
+            laser.enabled = false;
+        }
+
+    }
+
+    private void LockOnTarget()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(dir);
+    }
+
+    private void ShootLaser()
+    {
+        laser.enabled = true;
+        laser.SetPosition(0, laserTip.transform.position);
+        laser.SetPosition(1, player.transform.position);
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/SlimeEnemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlimeEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private NavMeshAgent agent;
+    private Rigidbody rigidbody;
+
+    public float health = 75f;
+    public float damage = 85f;
+    [SerializeField] private float speed = 3f;
+    private float distance;
+    private float minDistance = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player comes into distance than follow player
+        distance = Vector3.Distance(player.transform.position, transform.position);
+        if( distance <= minDistance )
+        {
+            transform.LookAt(player.transform.position);
+            agent.destination = player.transform.position;
+        }
+        // if enemy health below 0 die
+        if( health <= 0f )
+        {
+            enemyDead();
+        }
+    }
+
+    private void suicideKill()
+    {
+        // if player comes into attack range 
+        // attack player 
+        // destroy enemy
+    }
+
+    private void enemyDead()
+    {
+        // do dead animation
+        Destroy(gameObject);
+    }
+}
+
