32e2ed35986dc967d393e5922a3e2297927234bf
Kevin Jones <bioncl7@gmail.com>
2021-05-05 12:31:29 -0500

    Made level transitions work

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -12,41 +12,43 @@ public class LevelEnd : MonoBehaviour
 	Text timeText;
 
 	private void Start()
-    {
+	{
 		levelNumText = GameObject.Find("LevelNumText").GetComponent<Text>();
-		levelNumText.text = "" + (SceneManager.GetActiveScene().buildIndex + 1);
+		levelNumText.text = "Level " + SceneManager.GetActiveScene().buildIndex + " Complete";
 		levelObject = GameObject.Find("Level");
 		timeText = GameObject.Find("TimeText").GetComponent<Text>();
 		levelObject.SetActive(false);
 	}
-    public void OnCollisionEnter(Collision collision) {
+
+	public void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.tag == "Player") {
 			levelObject.SetActive(true);
 			timeText.text = "Time: " + GetTime(Time.time);
 			collision.gameObject.GetComponent<PlayerMovement>().enabled = false;
-			//Time.timeScale = 0f;
+			NextLevel();
 		}
 	}
 
 	// answers.unity.com/questions/45676/making-a-timer-0000-minutes-and-seconds.html
 	string GetTime(float t)
-    {
+	{
 		string minutes = Mathf.Floor(t / 60).ToString("00");
 		string seconds = (t % 60).ToString("00");
 		return string.Format("{0}:{1}", minutes, seconds);
 	}
 
 	public void NextLevel()
-    {
-		if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCount - 1)
-			nextScene = SceneManager.GetActiveScene().buildIndex + 1;
-		else nextScene = 0;
-		StartCoroutine( loadNextScene() );
-    }
-
-	IEnumerator loadNextScene() 
 	{
-		Debug.Log("Next Level");
+		if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings) {
+			nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+		} else {
+			nextScene = 0;
+		}
+		StartCoroutine( loadNextScene() );
+	}
+
+	IEnumerator loadNextScene()
+	{
 		yield return new WaitForSeconds(3);
 		SceneManager.LoadScene(nextScene);
 
