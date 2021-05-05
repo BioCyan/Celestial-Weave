8d05e3a80e195406c09989ca7e5332aa045a784b
andreas-ag <qhm389@my.utsa.edu>
2021-05-05 00:29:13 -0500

    Level1Tweaks
    title

---
+++ Assets/Scripts/WorldScripts/CanvasLookAtPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasLookAtPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform playerCam;
+    [SerializeField] private float viewDistance = 10f;
+    void Start()
+    {
+        if (playerCam == null)
+            playerCam = GameObject.FindGameObjectWithTag("Player").transform.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dis = (transform.position - playerCam.position).magnitude;
+        if (dis < viewDistance)
+        {
+            GetComponent<Canvas>().enabled = true;
+            transform.LookAt(2 * transform.position - playerCam.position);
+        }
+        else
+            GetComponent<Canvas>().enabled = false;
+    }
+}
