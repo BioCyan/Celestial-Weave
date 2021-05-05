aa5da35b424367d2139ee83a83457d037b3763d2
andreas-ag <qhm389@my.utsa.edu>
2021-05-05 01:56:56 -0500

    PrefabCommit
    dkfsa

--- Assets/Scripts/WorldScripts/GravityController.cs
+++ Assets/Scripts/WorldScripts/GravityController.cs
@@ -9,8 +9,10 @@ public class GravityController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("JUMP");
+        Vector3 upVec = new Vector3(0, transform.rotation.y, 0).normalized;
         float force = Mathf.Sqrt(2 * height * 9.8f) * other.GetComponent<Rigidbody>().mass;
-        other.gameObject.GetComponent<Rigidbody>().AddForce(force * transform.up, ForceMode.Impulse);
+        other.gameObject.GetComponent<Rigidbody>().AddForce(force * upVec, ForceMode.Impulse);
         GetComponent<AudioSource>().Play();
+        Debug.Log(upVec.ToString() + " " + Vector3.up);
     }
 }
